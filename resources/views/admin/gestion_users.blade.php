01aeb68fe57a4c80da863d42b65c41f54e8b26f8
Nassur <abdelnassurr@gmail.com>
2023-02-23 17:43:25 +0000

    bozartsvision

--- resources/views/admin/gestion_users.blade.php
+++ resources/views/admin/gestion_users.blade.php
@@ -1,3 +1,60 @@
 <x-admin-basic>
-    <h1>LISTE DES UTILISATEURS il y'a {{ count($users) }} urers</h1>
+    <!-- MAIN CONTENT-->
+    <div class="main-content">
+        <div class="section__content section__content--p30">
+            <div class="container-fluid">
+                <div class="row m-t-30">
+                    <div class="col-md-12">
+                        <!-- DATA TABLE-->
+                        <div class="table-responsive m-b-40">
+                            <table class="table table-borderless table-data3">
+                                <thead>
+                                    <tr>
+                                        <th>Prenom</th>
+                                        <th>Téléphone</th>
+                                        <th>email</th>
+                                        <th>status</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($users as $user)
+                                    <tr>
+                                        <td> {{ $user->prenom }} </td>
+                                        <td> {{ $user->telephone }} </td>
+                                        <td> {{ $user->email }} </td>
+                                        <td>
+                                            @if ($user->status)
+                                                <div class="card-body">
+                                                    <label class="switch switch-text switch-success switch-pill">
+                                                        <input type="checkbox" class="switch-input" checked="">
+                                                        <span data-on="On" data-off="Off" class="switch-label"></span>
+                                                        <span class="switch-handle"></span>
+                                                    </label>
+                                                </div>
+                                                @else
+                                                <div class="card-body">
+                                                    <label class="switch switch-text switch-success switch-pill">
+                                                        <input type="checkbox" class="switch-input" checked="true">
+                                                        <span data-on="On" data-off="Off" class="switch-label"></span>
+                                                        <span class="switch-handle"></span>
+                                                    </label>
+                                                </div>
+                                            @endif
+                                            
+                                        </td>
+                                        <td>
+                                            
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- END DATA TABLE-->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-admin-basic>
